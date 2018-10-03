dcc07cde7e7d93947cdb1b8644fe33d70e15fb84
Nguyen Viet Huan <ngnviethuan@gmail.com>
2018-10-03 15:19:08 +0700

    fix model-builder

--- src/models/model-builder.ts
+++ src/models/model-builder.ts
@@ -1,18 +1,24 @@
 import * as Sequeliz from 'sequelize';
 
-import * as userAssociations from './users.associate';
+import * as associations from './';
 
 export class ModelBuilder {
     constructor(private model: Sequeliz.Model<{}, any>) {}
 
     public userAssociate(rolesUsersModel: any, pondsModel: any, couponModel: any, boughtbreedsModel: any) {
-        userAssociations.userToRolesUser(this.model, rolesUsersModel);
-        userAssociations.userToPond(this.model, pondsModel);
-        userAssociations.userToCoupon(this.model, couponModel);
-        userAssociations.userToBoughtBreeds(this.model, boughtbreedsModel);
+        associations.userToRolesUser(this.model, rolesUsersModel);
+        associations.userToPond(this.model, pondsModel);
+        associations.userToCoupon(this.model, couponModel);
+        associations.userToBoughtBreeds(this.model, boughtbreedsModel);
     }
 
     public boughtbreeddetailsAssociate(boughtBreedsModel: any, breedsModel: any) {
-        //
+        associations.boughtbreeddetailsToBoughtBreeds(this.model, boughtBreedsModel);
+        associations.boughtbreeddetailsToBreeds(this.model, breedsModel);
+    }
+
+    public boughtbreedsAssociate(boughtBreedDetailsModel: any, usersModel: any) {
+        associations.boughtbreedsToBoughtBreedDetails(this.model, boughtBreedDetailsModel);
+        associations.boughtbreeddetailsToBreeds(this.model, usersModel);
     }
 }
