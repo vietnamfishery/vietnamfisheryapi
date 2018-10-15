aef7dcce807cf5c783aa425091bbae30c92a77e5
Phong Hoai Vo <vo.hoaiphongamkg@gmail.com>
2018-10-15 22:25:19 +0700

    edit services

--- src/services/stocking-details.services.ts
+++ src/services/stocking-details.services.ts
@@ -3,13 +3,9 @@ import { IOptionsModelDB } from '../interfaces';
 import { stockingdetailOptions } from '../models/objects';
 
 export class StockingDetailsServices extends BaseServices {
-    constructor(
-        protected optionsModel: IOptionsModelDB = {
-            name: stockingdetailOptions.tableName,
-            model: stockingdetailOptions.attributes,
-            deleteMode: stockingdetailOptions.options
-        }
-    ) {
-        super(optionsModel);
+    protected static optionsModel: IOptionsModelDB = stockingdetailOptions;
+    constructor() {
+        super(StockingDetailsServices.optionsModel);
+        this.models = this.conn.stockingdetailsModel;
     }
 }
